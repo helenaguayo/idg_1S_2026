182b0063e41e6967c87a2c7aa425188d746dbc29
helenaguayo <helen.aguayo@usach.cl>
2026-04-14 09:31:59 -0400

    Se añade archivos de primera clase de SQL

---
+++ data/sql/clase1.sql
@@ -0,0 +1,10 @@
+/* Comentario largo
+
+*/
+
+-- Comentario corto
+
+-- Consultar todos los registros de la tabla de artistas
+
+SELECT * -- Nombres de las columnas. "*" Es para llamar a todas las columnas.
+FROM "Artist"-- Nombre de la tabla
